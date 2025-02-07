179228ccc25bc15c9f18ce986d71685ce98daced
Christoph Gebhardt <cg3161@columbia.edu>
2025-02-07 10:31:36 -0500

    renamed modules

---
+++ code/raspberry_pis/picam_acquisition_modules/camera_acquisition.py
@@ -0,0 +1,117 @@
+import picamera
+import io, os
+import datetime as dt
+import time
+
+class TimeStamp(object):
+    """
+    Class for issuing timestamps and saving them to CSV files.
+
+    This class is designed for capturing timestamps from a picamera, specifically
+    the PTS (from GPU) and pi STC, and saving them into CSV files. It can be
+    used for video timestamp synchronization.
+
+    Parameters:
+    camera (Camera): The camera object providing frame data and timestamps.
+    video_filename (str): The filename for the video output file.
+    timestamp_filename (str): The filename for the CSV file to save timestamps.
+
+    Methods:
+    - write(buf): Write frame data and capture timestamps from the camera.
+    - flush(): Save the captured timestamps to a CSV file.
+    - close(): Close the video output file.
+
+    Example:
+    camera = Camera()
+    timestamp = TimeStamp(camera, "video_output.mp4", "timestamps.csv")
+    timestamp.write(frame_data)
+    timestamp.flush()
+    timestamp.close()
+    """
+    
+    def __init__(self, camera, video_filename, timestamp_filename):
+        self.camera         = camera
+        self._video_output  = io.open(video_filename, 'wb')
+        self._timestampFile = timestamp_filename
+        self._timestamps    = []
+        
+    def write(self, buf):
+        if self.camera.frame.complete is not None:
+            if self.camera.frame.timestamp is None: # the first I-Frame and all SPS-Headers do not possess timestamps and are thus flagged with a timestamp of -1000 
+               timestamp = -1000 
+            else:
+               timestamp = self.camera.frame.timestamp
+            
+            self._timestamps.append(
+                ( self.camera.frame.index, self.camera.frame.frame_type, timestamp, time.time() )
+                )
+        
+        return self._video_output.write(buf)
+    
+    def flush(self):
+        with io.open(self._timestampFile, 'w') as f:
+            f.write('index, frame_type, GPU Time, time.time()\n')
+            for entry in self._timestamps:
+                f.write('%d,%d,%d,%f\n' % entry)
+        
+    def close(self):
+        self._video.close()
+
+
+
+
+def RecordVideo(experiment_path, new_experimentID, configuration_parameters, FPS, RESOLUTION_PX, SHUTTERSPEED_US, CAMERA_MODE, BITRATE):
+
+    with picamera.PiCamera() as camera: 
+      
+        camera.resolution  = RESOLUTION_PX
+        camera.framerate   = FPS
+        camera.sensor_mode = CAMERA_MODE
+        
+        # preview while recording?
+        if configuration_parameters['camera_preview']:
+           camera.start_preview(fullscreen = False, window = (400, 70, 640, 320))  
+        
+        # timestamp settings
+        camera.annotate_background = picamera.Color('black')
+        camera.annotate_text       = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        
+        videoTime = time.strftime('%H%M%S')
+        
+        # create file names for videos
+        videoName  = new_experimentID + '_' + videoTime + ".h264"
+        path_video = os.path.join(experiment_path, videoName)
+        
+        if 'IR' in configuration_parameters['illumination']:  
+            
+            camera.iso           = 300
+            camera.shutter_speed = SHUTTERSPEED_US # in microseconds
+            camera.awb_mode      = 'off'
+            
+            if configuration_parameters['camera_num'] == ['1']:
+               rg , bg           = (0.8, 0.9) # automatic awb_mode is not working in NOIR picamera with firmware pre 07/2019!!!
+            
+            elif configuration_parameters['camera_num'] == ['2']:
+               rg , bg           = (0.9, 0.85)
+            
+            camera.awb_gains     = (rg,bg) 
+            camera.brightness    = 60               # [def 50,    0 100]
+            camera.contrast      = 80               # [def  0, -100 100]
+            
+    
+        elif 'RL' in configuration_parameters['illumination']:
+            camera.awb_mode = 'auto'
+        
+        output = TimeStamp(camera, path_video, os.path.join(experiment_path, new_experimentID + '_clock.csv'))
+    
+        camera.start_recording(output, format = 'h264', bitrate = BITRATE)  
+        
+        start = dt.datetime.now()
+        while (dt.datetime.now() - start).seconds < configuration_parameters['video_duration_mins'] * 60:
+            camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')                                     
+            camera.annotate_text_size = 15
+    
+        camera.stop_recording()
+        
+        if configuration_parameters['camera_preview']:
+            camera.stop_preview()
---
+++ code/raspberry_pis/picam_acquisition_modules/experiment_manager.py
@@ -0,0 +1,105 @@
+import datetime as dt
+from pathlib import Path
+import sys
+
+from picam_acquisition_modules import config
+
+def create_experiment_folder(experimenter, exp_num, camera_num):
+    """
+    Create a new experiment folder for data storage.
+
+    This function creates a new experiment folder with a specific structure
+    for organizing and storing data related to behavioral experiments. It
+    ensures that the folder does not already exist and then proceeds to
+    create the necessary directory structure.
+
+    Parameters:
+    experimenter (str): The name of the experimenter conducting the experiment.
+    exp_num (str): The experiment number or identifier.
+    camera_num (list): A list of camera numbers used for the experiment.
+
+    Returns:
+    tuple: A tuple containing the experiment folder path and the experiment
+    identifier.
+
+    Example:
+    exp_path, new_expID = create_experiment_folder("John Doe", "01", [1, 2])
+    """
+    
+    # Create the root path
+    root_path = Path('/home/pi/Desktop/Behavior') / experimenter
+
+    # Create the experiment folder name
+    currentDate = dt.datetime.now().strftime("%Y%m%d")
+    new_expID   = f"{currentDate}_e{exp_num}_pic{camera_num[0]}"
+    exp_path    = root_path / new_expID
+
+    print(f'\nCreating new experiment folder for {new_expID}...')
+    
+    if exp_path.exists():
+        print('Experiment folder already exists. Script aborted!')
+        sys.exit()  # This is a hard exit without error collection
+    else:
+        exp_path.mkdir(parents=True, exist_ok=True)
+        print('Folder created!')
+
+    return (exp_path, new_expID)
+
+
+def write_metadata(exp_path, new_experimentID, configuration_parameters):
+    """
+    Create a metadata text file for experiment information.
+
+    This function generates a text file containing metadata related to an
+    experiment, including details such as the experimenter, animal ID, species,
+    camera settings, and more. The text file is named after the experiment's
+    unique identifier and is stored in the specified experiment path.
+
+    Parameters:
+    exp_path (str): The path where the metadata text file will be saved.
+
+    Returns:
+    None
+
+    Example:
+    write_metadata("/home/pi/experiments/experiment_01")
+    """
+    
+    metadata = {
+        'script_used': configuration_parameters['script_name'],
+        'experimenter': configuration_parameters['experimenter'],
+        'animalID': configuration_parameters['animalID'],
+        'speciesID': configuration_parameters['speciesID'],
+        'sex': configuration_parameters['sex'],
+        'repro_state': configuration_parameters['repro_state'],
+        'pup_age_days': configuration_parameters['pup_age_days'],
+        'litter': configuration_parameters['litter'],
+        'exp_num': configuration_parameters['exp_num'],
+        'camera_num': configuration_parameters['camera_num'][0],
+        'view': configuration_parameters['view'],
+        'illumination': configuration_parameters['illumination'],
+        'behavior_paradigm': configuration_parameters['behavior_paradigm'],
+        'photometry': configuration_parameters['photometry'],
+        'viral_injection_date': configuration_parameters['viral_injection_date'],
+        'virus_id': configuration_parameters['virus_id'],
+        'virus_target': configuration_parameters['virus_target'],
+        'video_duration_mins': configuration_parameters['video_duration_mins'],
+        'fps': configuration_parameters['FPS'],
+        'resolution_px': configuration_parameters['RESOLUTION_PX'],
+        'shutterspeed_us': configuration_parameters['SHUTTERSPEED_US'],
+        'bitrate': configuration_parameters['BITRATE']
+    }
+
+    meta_fname      = f"{new_experimentID}.txt"
+    metadata_lines  = [f"{new_experimentID}"]  # Include the expID identifier without key
+    metadata_lines += [f"{key}:{value}" for key, value in metadata.items()]
+
+    metadata_text = "\n".join(metadata_lines)
+
+    try:
+        with open(exp_path / meta_fname, "w") as file:
+            file.write(metadata_text)
+        print('\nTxt-file for Metadata created!')
+
+    except Exception as e:
+        print(f'\nCould not save txt-file for Metadata: {str(e)}')
